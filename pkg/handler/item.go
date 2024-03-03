8cc58d5064a81fd765e1ee87ff82d03b595d8cfb
smmlg <145594496+smmlg@users.noreply.github.com>
2024-03-03 20:54:59 +0500

    Add files via upload

---
+++ pkg/handler/item.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	todo "todo-app"
+)
+
+func (h *Handler) createItem(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	listId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid list id param")
+		return
+	}
+
+	var input todo.TodoItem
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.TodoItem.Create(userId, listId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+}
+
+func (h *Handler) getAllItems(c *gin.Context) {
+
+}
+func (h *Handler) getItemById(c *gin.Context) {
+
+}
+func (h *Handler) updateItem(c *gin.Context) {
+
+}
+func (h *Handler) deleteItem(c *gin.Context) {
+
+}
